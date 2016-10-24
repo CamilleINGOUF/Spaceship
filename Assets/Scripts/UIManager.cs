c26645c006d967b2041d7f1843cfc2a146a8d925
CamilleINGOUF <ingouf.camille@gmail.com>
2016-10-25 01:34:47 +0200

    commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        //Application.LoadLevel("Game");
+        SceneManager.LoadScene("SpaceGame");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    	
+}
